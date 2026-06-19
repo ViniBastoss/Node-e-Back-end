74b32a72d9d5380697a393f684290265a0b5868b
ViniBastoss <vinibastos31@gmail.com>
2026-06-19 00:08:36 -0300

    att

--- app_expressjs/Controller/clientsController.ts
+++ app_expressjs/Controller/clientsController.ts
@@ -30,6 +30,27 @@ async function store (req: Request, res: Response, next: any){
     const client = await clientsModel.findByPk(req.params.id);
    res.render("edit", {client: client});
     }
-   
-    export default { index, create, store,show, edit};
+   async function update(req: Request, res: Response, next: any){
+    await clientsModel.update(
+        req.body as IClients, {
+            where: {
+                id: req.params.id
+            }
+        }
+    );
+    res.redirect("/");
+}
+    async function del(req: Request, res: Response, next: any){
+        await clientsModel.destroy(
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        )
+         res.redirect("/");
+    }
+
+
+    export default { index, create, store,show, edit, update, del}
 
--- app_expressjs/Router/clients.ts
+++ app_expressjs/Router/clients.ts
@@ -7,15 +7,22 @@ const router = Router();
 
 
 router.get('/',clientsController.index);
-router.get('/:id',clientsController.show);
+
 
 router.get('/create',clientsController.create);
 router.post("/create", clientsController.store);
 router.get("/edit/:id", clientsController.edit);
+router.post("/edit/:id", clientsController.update);
+router.get("/del/:id", clientsController.del);
+
    
+   
+
 
 router.get('/sobrenos',(req, res)=>{
     res.send("<h2>Rota sobre nós</h2>")
 })
+
+router.get('/:id',clientsController.show);
 
 export default router;;
