93d18565d0fc56b403a0b1b3a568eb9a75002ba6
ViniBastoss <vinibastos31@gmail.com>
2026-06-23 01:40:00 -0300

    att

--- app_expressjs/Controller/usersController.ts
+++ app_expressjs/Controller/usersController.ts
@@ -11,26 +11,29 @@ async function checkLogin (req: Request, res: Response, next: any){
 
     const login = req.body as IUsers;
 
-    let logado = await usersModel.findOne({
+    try{
+         let logado = await usersModel.findOne({
         where:{
             user: login.user,
             password: login.password
         }
     });
 
-    if(logado != null){
+     if(logado != null){
         res.redirect("/clients")
         //OK!
     }
     else{
-        console.log("Senha inválida!")
-        //NOT OK
+        //console.log("Senha inválida!")
+        throw new Error("Senha inválida!!!")
     }
 
+    }catch(erro){
+        console.log(erro);
+        res.status(500).end();
+
+    }
 }
-
-
-
 
  export default { login, checkLogin}
 
